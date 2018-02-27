e4fb165ce1d825757c8d4399271e2e403b2c18aa
bowler229 <298.bowl@gmail.com>
2018-02-26 23:21:21 -0500

    More additions to main homepage

--- inc/head.php
+++ inc/head.php
@@ -3,13 +3,16 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sample page</title>
+    <title>TapeKings | <?php  ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Place favicon.ico in the root directory -->
     <link rel="stylesheet" href="lib/normalize/normalize.css">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap-grid.css">
+    <!-- <link rel="stylesheet" href="css/bootstrap/bootstrap.css"> -->
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
 
   </head>
--- index.php
+++ index.php
@@ -7,10 +7,98 @@
 
     <!-- Add your site or application content here -->
     <?php include 'inc/nav.php'; ?>
-
     <div class="hero-img">
       <img src="css/img/hero-sports-home.jpg" class="hero" alt="">
     </div>
+    <div class="container">
+      <div class="row main-sports">
+        <div class="col-sm-12">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="baseball-option">
+                <a href="#">
+                  <img src="css/img/baseball-option.png" class="img-fluid" alt="">
+                  <!-- <div class="sports-options-title-overlay">
+                    <span>BASEBALL</span>
+                  </div> -->
+                </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="softball-option">
+                <a href="#">
+                  <img src="css/img/softball-option.png" class="img-fluid" alt="">
+                  <!-- <div class="sports-options-title-overlay">
+                    <span>BASEBALL</span>
+                  </div> -->
+                </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="endless-possibilities">
+                <a href="#">
+                  <img src="http://via.placeholder.com/470x535" class="img-fluid ep" alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      <!-- </div>
+      <div class="row alt-sports"> -->
+        <div class="col-sm-8">
+          <div class="row alt-sports">
+            <div class="col-sm-4">
+              <div class="hockey-option">
+                <a href="#">
+                  <img src="css/img/hockey-option.png" class="img-fluid" alt="">
+                  <!-- <div class="sports-options-title-overlay">
+                    <span>BASEBALL</span>
+                  </div> -->
+                </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="tennis-option">
+                <a href="#">
+                  <img src="css/img/tennis-option.png" class="img-fluid" alt="">
+                  <!-- <div class="sports-options-title-overlay">
+                    <span>BASEBALL</span>
+                  </div> -->
+                </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="lacrosse-option">
+                <a href="#">
+                  <img src="css/img/lacrosse-option.png" class="img-fluid" alt="">
+                  <!-- <div class="sports-options-title-overlay">
+                    <span>BASEBALL</span>
+                  </div> -->
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row creations">
+        <div class="col-sm-12">
+          <h1>OUR LATEST CREATIONS</h1>
+          <h5>Follow us on Instagram @tapekings</h5>
+        </div>
+        <div class="col-sm-10 offset-1">
+          <!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/475b8f732dec5a93932d549faad0c1f5.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="banner difference">
+        <img src="css/img/the-difference.jpg" alt="">
+        <div class="container">
+          <h2>The Difference</h2>
+        </div>
+      </div>
+    </div>
+
 
 
 
